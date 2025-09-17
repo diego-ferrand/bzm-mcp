a77d01941a00e195b76daa3153f25779460dccc6
David <3dgiordano@gmail.com>
2025-09-17 11:33:38 -0300

    MOB-43221: Add locations support to configure (#20)

--- models/performance_test.py
+++ models/performance_test.py
@@ -15,13 +15,25 @@ class PerformanceTestObject:
     @staticmethod
     def _extract_override_execution(test_data: Dict[str, Any]) -> Dict[str, Any]:
         execution_config = {}
-        for param in ["iterations", "concurrency", "hold-for", "ramp-up", "steps", "executor"]:
+        concurrency = int(test_data["concurrency"]) if "concurrency" in test_data and test_data["concurrency"] else 1
+        for param in ["iterations", "concurrency", "hold-for", "ramp-up", "steps", "executor", "locations"]:
             if param in test_data:
                 match param:
                     case "ramp-up":
                         execution_config["rampUp"] = test_data[param]
                     case "hold-for":
                         execution_config["holdFor"] = test_data[param]
+                    case "locations":
+                        locations_percents = {}
+                        locations_concurrency = {}
+                        for location in test_data[param]:
+                            location_kv = location.split("=")
+                            location_percent = int(location_kv[1])
+                            locations_percents[location_kv[0]] = location_percent
+                            locations_concurrency[location_kv[0]] = int(location_percent * concurrency / 100)
+
+                        execution_config["locations"] = locations_concurrency
+                        execution_config["locationsPercents"] = locations_percents
                     case _:
                         execution_config[param] = test_data[param]
         return execution_config
