862ca5d7ab892ca1c19ce6587c8feedd8e9725c8
David <3dgiordano@gmail.com>
2025-09-24 15:25:39 -0300

    MOB-43433: Allow any type of data on override_executions
    Exist some test with strange data configured inside override_executions, it's better to allow Optional[Any]

--- models/test.py
+++ models/test.py
@@ -12,4 +12,4 @@ class Test(BaseModel):
     updated: Optional[str] = Field(description="The datetime that the test was updated", default=None)
     project_id: int = Field(description="The Project ID")
     configuration: Dict[str, Any] = Field(description="Contains all the advanced BlazeMeter related configurations")
-    override_executions: List[Dict[str, Any]] = Field(description="The test settings used when running the test in BlazeMeter")
+    override_executions: List[Optional[Any]] = Field(description="The test settings used when running the test in BlazeMeter")
