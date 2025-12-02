fec460d886c485ffb4f8e0ace2b6c2c2c6d42e5a
David <3dgiordano@gmail.com>
2025-12-02 11:54:54 -0300

    Help manager and tools

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from tools.user_manager import register as register_user_manager
-from tools.project_manager import register as register_project_manager
-from tools.workspace_manager import register as register_workspace_manager
-from tools.test_manager import register as register_test_manager
-from tools.execution_manager import register as register_execution_manager
-from tools.account_manager import register as register_account_manager
-from config.token import BzmToken
 from typing import Optional
+
+from config.token import BzmToken
+from tools.account_manager import register as register_account_manager
+from tools.execution_manager import register as register_execution_manager
+from tools.help_manager import register as register_help_manager
+from tools.project_manager import register as register_project_manager
+from tools.test_manager import register as register_test_manager
+from tools.user_manager import register as register_user_manager
+from tools.workspace_manager import register as register_workspace_manager
+
 
 def register_tools(mcp, token: Optional[BzmToken]):
     """
@@ -21,3 +24,4 @@ def register_tools(mcp, token: Optional[BzmToken]):
     register_test_manager(mcp, token)
     register_execution_manager(mcp, token)
     register_account_manager(mcp, token)
+    register_help_manager(mcp, token)
