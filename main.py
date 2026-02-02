a75062fa14297bc7acb85ca3fd90318cd53817af
Diego Ferrand <diego.ferrand@abstracta.com.uy>
2026-02-02 13:15:54 -0300

    Update license (#44)
    * Added license to each .py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2025 Perforce Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import argparse
 import json
 import logging
@@ -167,6 +182,8 @@ def main():
             " |____/|_|\__,_/___\___||_|  |_|\___|\__\___|_|   \n"
             "                                                    \n"
             f" BlazeMeter MCP Server v{__version__} \n"
+            " Copyright © 2025, Perforce Software, Inc. All rights reserved.\n"
+            " Licensed under the Apache License, Version 2.0.\n"
         )
         print(logo_ascii)
         if sys.platform == "darwin" and __bundle__.endswith(".app"):
